e5352e873d4b7b32900a9c2c9840e18070f37b23
NachoBC82 <nachobc82@gmail.com>
2026-06-10 00:02:52 +0200

    [Dev] Animation for dialog text add

--- Scripts/dialog_box.gd
+++ Scripts/dialog_box.gd
@@ -1,14 +1,27 @@
 extends Control
 
 @onready var dialog_line = %DialogLine
-@onready var speaker = %DialogSpeaker
+@onready var speaker_name = %DialogSpeaker
 
+const Animation_speed : int = 30
+var animate_text : bool = false
+var current_visible_characters : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+func _process(delta: float):
+	if animate_text:
+		if dialog_line.visible_ratio < 1:
+			dialog_line.visible_ratio += (1.0/dialog_line.text.length()) * (Animation_speed * delta)
+		else:
+			animate_text = false 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func change_line(speaker:String, line: String):
+	speaker_name.text = speaker
+	current_visible_characters = 0
+	
+	dialog_line.text = line
+	dialog_line.visible_characters = 0
+	animate_text = true
--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -5,9 +5,9 @@ extends Node2D
 var dialog_index : int
 
 const dialog_lines : Array[String] = [
-	"Lex: Otro día más, otro caso más. Encima hoy toca conocer a mi nuevo compañero.",
-	"Lex: Encima todos están obsesionados con esas tecnologías inútiles.",
-	"Policia: Buenas Lex, la víctima está por aquí. Tu nuevo compañero ya la está observando."
+	"Lex: Otro dia más, otro caso mas. Por si fuera poco hoy toca conocer a mi nuevo compañero.",
+	"Lex: Encima todos están obsesionados con esas tecnologias inutiles.",
+	"Policia: Buenas Lex, la víctima está por aqui. Tu nuevo compañero ya la está observando."
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -35,11 +35,8 @@ func parse_line(line: String):
 
 func process_current_line():
 	var line = dialog_lines[dialog_index]
-	var line_speaker = parse_line(line)["speaker"]
-	var line_info = parse_line(line)["dialog_line"]
+	var line_info = parse_line(line)		
 		
-	dialog_ui.speaker.text = line_speaker
-	character.change_character(line_speaker)
-	
-	dialog_ui.dialog_line.text = line_info
+	dialog_ui.change_line(line_info["speaker"],line_info["dialog_line"])
+	character.change_character(line_info["speaker"])
 	
